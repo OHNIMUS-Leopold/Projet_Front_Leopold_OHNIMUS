f9d420edd86bfa9dbb7d8ce586742867d80600d5
Léopold OHNIMUS <leopold.ohnimus@edu.univ-fcomte.fr>
2024-11-26 13:35:44 +0100

    rule eslint

--- eslint.config.js
+++ eslint.config.js
@@ -12,18 +12,14 @@ export default [
     name: 'app/files-to-ignore',
     ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
   },
-  {
-    rules: {
-      'vue/multi-word-component-names': [
-        'error',
-        {
-          ignores: [],
-        },
-      ],
-    },
-  },
-
+  
   ...pluginVue.configs['flat/essential'],
   ...vueTsEslintConfig(),
   skipFormatting,
+
+  {
+    rules: {
+      'vue/multi-word-component-names': 'off', 
+    },
+  },
 ]
