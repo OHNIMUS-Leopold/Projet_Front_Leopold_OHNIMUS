8ee5dff5d2aee8fba2d53562fa83b3d79543aa2c
Léopold OHNIMUS <leopold.ohnimus@edu.univ-fcomte.fr>
2024-12-02 11:09:56 +0100

    fondations en variables globales

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,19 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
+  modules: ['@nuxt/eslint'],
+
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @use "@/scss/foundations/variables" as *;
+            @use "@/scss/foundations/functions" as *;
+            @use "@/scss/foundations/mixins" as *;
+          `,
+        },
+      },
+    },
+  }
 })
