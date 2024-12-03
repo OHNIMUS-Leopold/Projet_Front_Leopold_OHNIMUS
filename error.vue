5f4d7c60a0ba931b9dfe9e3cabf95449ab0f04d3
Léopold OHNIMUS <leopold.ohnimus@edu.univ-fcomte.fr>
2024-12-03 15:07:25 +0100

    page d'erreur

---
+++ error.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import type { NuxtError } from "#app";
+
+defineProps({ 
+    error: {
+        type: Object as () => NuxtError,
+        default: null
+    }});
+
+const handleError = () => clearError({redirect: '/'} );
+
+</script>
+
+<template>
+    <div>
+        <p>Code d'erreur : {{ error.statusCode }}</p>
+        <p>Message : {{ error.statusMessage }}</p>
+        <button @click="handleError">Retourner à l'accueil</button>
+    </div>
+</template>
