5713f9a71b19ec6d3f4b6e6b06060c611e599e5d
Léopold OHNIMUS <leopold.ohnimus@edu.univ-fcomte.fr>
2024-12-03 13:21:13 +0100

    affichage liste de posts

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -1,3 +1,21 @@
+<script setup lang="ts">
+import type { SanityDocument } from "@sanity/client";
+// import imageUrlBuilder from "@sanity/image-url";
+// import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
+
+// Query GROQ pour sanity
+const POST_QUERY = groq`*[_type == "post" && slug.current == $slug][0]`;
+const route = useRoute();
+
+// Le paramètre de la route nuxt est envoyé dans la requête sanity
+const { data: post } = await useSanityQuery<SanityDocument>(POST_QUERY, {slug: route.params.slug});
+
+
+
+</script>
+
 <template>
-    <p>slug</p>
+    <div>
+        {{ post }}
+    </div>
 </template>
