0fbc13f789fba3ccb9419609dfdbd9076f7520a6
Léopold OHNIMUS <leopold.ohnimus@edu.univ-fcomte.fr>
2024-12-10 15:03:10 +0100

    fonction de suppression d'habitude

--- pages/app/dashboard.vue
+++ pages/app/dashboard.vue
@@ -15,6 +15,28 @@ const { data: data, refresh: refreshDashboard, error } = useAsyncData('dashboard
 
     return response.json();
 });
+
+
+
+async function deletePersonalHabit(habitId: string) {
+    try {
+        const response = await fetch(`http://localhost:4000/habits/${habitId}`, {
+            method: 'DELETE',
+            headers: {
+                'Authorization': `Bearer ${useCookie('api_tracking').value}`,
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (!response.ok) {
+            throw new Error('Erreur lors de la suppression de l\'habitude');
+        }
+
+        await refreshDashboard();
+    } catch (error) {
+        console.error('Erreur:', error);
+    }
+}
 </script>
 
 <template>
@@ -33,6 +55,7 @@ const { data: data, refresh: refreshDashboard, error } = useAsyncData('dashboard
         <ul>
             <li v-for="(habit, index) in data.personalHabits" :key="index">
                 {{ habit.title }} : {{ habit.description }}
+                <button @click="deletePersonalHabit(habit.id)">Supprimer</button>
             </li>
         </ul>
 
