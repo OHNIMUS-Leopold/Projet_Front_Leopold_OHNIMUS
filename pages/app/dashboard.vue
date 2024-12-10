d8d02051a7fb41fa7fb6fe3674283651c489a5b0
Léopold OHNIMUS <leopold.ohnimus@edu.univ-fcomte.fr>
2024-12-10 14:46:57 +0100

    appel du frmulaire et maj de la liste

--- pages/app/dashboard.vue
+++ pages/app/dashboard.vue
@@ -1,13 +1,20 @@
 <script setup lang="ts">
-const response = await fetch('http://localhost:4000/dashboard', {
-    method: 'GET',
-    headers: {
-        'Authorization': `Bearer ${useCookie('api_tracking').value}`,
-        'Content-Type': 'application/json',
-    },
-});
 
-const data = await response.json();
+const { data: data, refresh: refreshDashboard, error } = useAsyncData('dashboard', async () => {
+    const response = await fetch('http://localhost:4000/dashboard', {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${useCookie('api_tracking').value}`,
+            'Content-Type': 'application/json',
+        },
+    });
+
+    if (!response.ok) {
+        throw new Error('Erreur lors de la récupération des données');
+    }
+
+    return response.json();
+});
 </script>
 
 <template>
@@ -28,5 +35,8 @@ const data = await response.json();
                 {{ habit.title }} : {{ habit.description }}
             </li>
         </ul>
+
+        <AddHabitsForm :refreshDashboard="refreshDashboard" />
+
     </div>
 </template>
