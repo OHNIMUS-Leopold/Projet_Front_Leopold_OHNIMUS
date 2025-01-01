4f3e2a61962bdb5f5d2a678696d9f5bce4ee22e6
Léopold OHNIMUS <leopold.ohnimus@edu.univ-fcomte.fr>
2025-01-01 15:04:09 +0100

    styling

--- components/Header.vue
+++ components/Header.vue
@@ -1,12 +1,16 @@
 <script setup lang="ts">
 </script>
 
+
 <template>
     <header class="header">
-        <nav class="header__nav">
-            <NuxtLink to="/" class="header__nav-link">home</NuxtLink>
-            <NuxtLink to="/about" class="header__nav-link">about</NuxtLink>
-            <NuxtLink to="/blog" class="header__nav-link">blog</NuxtLink>
+        <nav class="header__nav -static">
+            <NuxtLink to="/" class="header__nav-link -title">TrackingApp</NuxtLink>
+            <NuxtLink to="/" class="header__nav-link">Home</NuxtLink>
+            <NuxtLink to="/about" class="header__nav-link">Pricing</NuxtLink>
+            <NuxtLink to="/blog" class="header__nav-link">Blog</NuxtLink>
+        </nav>
+        <nav class="header__nav -user">
             <NuxtLink to="/login" class="header__nav-link">
                 <Button class="button -outline">
                     Login
@@ -22,6 +26,39 @@
     </header>
 </template>
 
+
 <style lang="scss">
+.header {
+    display: flex;
+    padding: 16px;
+    background-color: $white;
+    justify-content: space-between;
+
+    &__nav {
+        display: flex;
+        align-items: center;
+        margin: auto;
+        gap: 1rem;
+    }
+
+    &__static-links,
+    &__user-links {
+        display: flex;
+        gap: 1rem;
+    }
+
+    &__nav-link {
+        text-decoration: none;
+        color: $gray800;
+        font-size: 16px;
+
+        &.-title {
+            font-size: 24px;
+            font-weight: bold;
+            padding-right: 48px;
+        }
+    }
+}
+
 
 </style>
