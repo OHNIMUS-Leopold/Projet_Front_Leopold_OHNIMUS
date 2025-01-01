8972ad5058833dfa4beec81dd7c7e403b3faf70b
Léopold OHNIMUS <leopold.ohnimus@edu.univ-fcomte.fr>
2025-01-01 18:01:52 +0100

    section hero bg image

--- components/Hero.vue
+++ components/Hero.vue
@@ -19,26 +19,43 @@ interface HeroProps {
 <template>
     <section v-if="hero" class="hero">
         <img v-if="hero.image" class="hero__image" :src="urlFor(hero.image)!.url()" alt="">
-        <h1 class="hero__title">{{ hero.title }}</h1>
-        <p class="hero__text">{{ hero.text }}</p>
-        <Button class="hero__button">{{ hero.trial }}</Button>
-        <ul class="hero__stats">
-            <li v-for="stat in hero.stats" :key="stat.value" class="hero__stat">
-                <strong class="hero__stat-value">{{ stat.value }}</strong> - <span class="hero__stat-text">{{ stat.text }}</span>
-            </li>
-        </ul>
+        <div class="hero__content">
+            <h1 class="hero__title">{{ hero.title }}</h1>
+            <p class="hero__text">{{ hero.text }}</p>
+            <Button class="hero__button">{{ hero.trial }}</Button>
+            <ul class="hero__stats">
+                <li v-for="stat in hero.stats" :key="stat.value" class="hero__stat">
+                    <strong class="hero__stat-value">{{ stat.value }}</strong> - <span class="hero__stat-text">{{ stat.text }}</span>
+                </li>
+            </ul>
+        </div>
     </section>
 </template>
 
 
 <style lang="scss">
 
-.hero__image {
-    position: absolute;
-    top: 0;
-    right: 0;
+.hero {
+    position: relative;
     width: 100%;
-    z-index: -1;
+    height: calc(100vh - 80px);
+    overflow: hidden;
+    color: $gray800; 
+
+    &__image {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        z-index: -1; 
+    }
+
+    &__content {
+        position: relative;
+    }
 }
+
 
 </style>
