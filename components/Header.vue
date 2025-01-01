c7a027727120e03cbcbc7d925db2ddb37c0d847d
Léopold OHNIMUS <leopold.ohnimus@edu.univ-fcomte.fr>
2025-01-01 14:39:06 +0100

    déplacement du header en composant

---
+++ components/Header.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <header>
+        <nav>
+            <NuxtLink to="/">home</NuxtLink>
+            <NuxtLink to="/about">about</NuxtLink>
+            <NuxtLink to="/blog">blog</NuxtLink>
+            <NuxtLink to="/login">
+                <Button class=" -outline">
+                    Login
+                </Button>
+            </NuxtLink>
+            <NuxtLink to="/register">
+                <Button class="-primary">
+                    Register
+                </Button>
+            </NuxtLink>
+            <!-- <NuxtLink to="/app/dashboard">dashboard</NuxtLink> -->
+        </nav>
+    </header>
+</template>
+
+<style lang="scss">
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,6 @@
 <template>
     <div class="l-default">
-        <div>
-            <NuxtLink to="/">home</NuxtLink>
-            <NuxtLink to="/about">about</NuxtLink>
-            <NuxtLink to="/blog">blog</NuxtLink>
-            <NuxtLink to="/login">login</NuxtLink>
-            <NuxtLink to="/register">register</NuxtLink>
-            <NuxtLink to="/app/dashboard">dashboard</NuxtLink>
-        </div>
+        <Header />
         <slot />
     </div>
 </template>
